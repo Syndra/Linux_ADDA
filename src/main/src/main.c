03ffaa9e4228af06ae69162c940230fff5b1df6f
lenny-kong <forlennykong@gmail.com>
2018-12-05 23:02:24 +0900

    Update main.c

--- src/main/src/main.c
+++ src/main/src/main.c
@@ -29,6 +29,36 @@ int main(){
 	while(1){
 		/* Sensing */
 		while((ADS1256_Scan() == 0));
+
+		/* TEST */
+		for (i = 0; i < ch_num; i++)
+		{
+			adc[i] = ADS1256_GetAdc(i);
+								 volt[i] = (adc[i] * 100) / 167;
+		}
+
+		for (i = 0; i < ch_num; i++)
+		{
+									buf[0] = ((uint32_t)adc[i] >> 16) & 0xFF;
+									buf[1] = ((uint32_t)adc[i] >> 8) & 0xFF;
+									buf[2] = ((uint32_t)adc[i] >> 0) & 0xFF;
+									printf("%d=%02X%02X%02X, %8ld", (int)i, (int)buf[0],
+												 (int)buf[1], (int)buf[2], (long)adc[i]);
+
+									iTemp = volt[i];	/* uV  */
+					if (iTemp < 0)
+					{
+						iTemp = -iTemp;
+														printf(" (-%ld.%03ld %03ld V) \r\n", iTemp /1000000, (iTemp%1000000)/1000, iTemp%1000);
+					}
+					else
+					{
+														printf(" ( %ld.%03ld %03ld V) \r\n", iTemp /1000000, (iTemp%1000000)/1000, iTemp%1000);
+					}
+
+		}
+
+
 		for (i = 0; i < nData; i++)
 		{
 			sensor_data_set(data[i], "???", SENSOR_DATA_TYPE_DOUBLE, ADS1256_GetAdc(i) /1670.0, "mV");
