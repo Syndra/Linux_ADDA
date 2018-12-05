2bcf532964a0e9799e05f30dc39a96874218c953
lenny-kong <forlennykong@gmail.com>
2018-12-05 22:43:54 +0900

    asdasd
    asdad

---
+++ src/monitor/src/bmp180_monitor.c
@@ -0,0 +1,8 @@
+#include <bmp180_monitor.h>
+#include <stdio.h>
+
+void monitor(float temp, float press, float alti) {
+	printf("\nTemperature : %.2f C\n",temp);
+	printf("Pressure :    %.2f Pa\n",press);
+	printf("Altitude :    %.2f h\n",alti);
+}
