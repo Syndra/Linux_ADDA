2ed35c1f57c48f91c07abe32b2fb196fb8a588b9
lenny-kong <forlennykong@gmail.com>
2018-12-07 19:22:30 +0900

    upup

--- timer_example/timer.c
+++ timer_example/timer.c
@@ -50,58 +50,8 @@ void time_register(timer_t* timerID, int _sec, int _msec, int _interval){
 	timer_settime(*timerID, 0, &its, NULL);
 }
 
-void set_timer(){
-
-}
-
-void timer(){
-	puts("Hi");
-}
-
-void run_timer(timer_t* timer, void* func, int _sec, int _msec, int _interval)
+void run_timer(timer_t* timerID, void* func, int _sec, int _msec, int _interval)
 {
 	function_register(&timerID, func);
 	time_register(&timerID, _sec, _msec, _interval);
-}
-
-int main(){
-
-	timer_t timerID = timer_init();
-	function_register(&timerID, timer);
-	time_register(&timerID, 1, 0, 1000000);
-
-	// struct sigevent         te;
-	// struct itimerspec       its;
-	// struct sigaction        sa;
-	// int                     sigNo = SIGRTMIN;
-	// int			sec = 1;
-	// int			msec = 0;
-	//
-	// /* Set up signal handler. */
-	// sa.sa_flags = SA_SIGINFO;
-	// sa.sa_sigaction = timer;     // 타이머 호출시 호출할 함수
-	// sigemptyset(&sa.sa_mask);
-	//
-	// if (sigaction(sigNo, &sa, NULL) == -1)
-	// {
-	// 	printf("sigaction error\n");
-	// 	return -1;
-	// }
-	//
-	// /* Set and enable alarm */
-	// te.sigev_notify = SIGEV_SIGNAL;
-	// te.sigev_signo = sigNo;
-	// te.sigev_value.sival_ptr = &timerID;
-	// timer_create(CLOCK_REALTIME, &te, &timerID);
-	//
-	// its.it_interval.tv_sec = sec;
-	// its.it_interval.tv_nsec = msec * 1000000;
-	// its.it_value.tv_sec = sec;
-	//
-	// its.it_value.tv_nsec = msec * 1000000;
-	// timer_settime(timerID, 0, &its, NULL);
-
-	while(1);
-
-	return 0;
 }
--- timer_example/timer.h
+++ timer_example/timer.h
@@ -19,6 +19,5 @@ timer_t timer_init();
 void timer_deinit();
 void function_register(timer_t* timerID, void* function);
 void time_register(timer_t* timerID, int _sec, int _msec, int _interval);
-void timer();
-void run_timer(timer_t* timer, void* func, int _sec, int _msec, int _interval);
+void run_timer(timer_t* timerID, void* func, int _sec, int _msec, int _interval);
 #endif
