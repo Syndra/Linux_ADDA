ffee4ea1b259f726666eb1779abc3b0a0d15755f
lenny-kong <forlennykong@gmail.com>
2018-12-07 19:46:41 +0900

    Update timer.h

--- src/timer/src/timer.h
+++ src/timer/src/timer.h
@@ -19,5 +19,5 @@ timer_t timer_init();
 void timer_deinit();
 void function_register(timer_t* timerID, void* function);
 void time_register(timer_t* timerID, int _sec, int _msec, int _interval);
-void run_timer(timer_t* timerID, void* func, int _sec, int _msec, int _interval);
+void run_timer(timer_t timerID, void* func, int _sec, int _msec, int _interval);
 #endif
