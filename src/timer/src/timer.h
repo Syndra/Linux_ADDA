74f3c2101372aa672f081b04d10d1d251e94dfb0
Lenny-kong <dadasa13@naver.com>
2018-12-07 01:25:28 -0900

    d

---
+++ src/timer/src/timer.h
@@ -0,0 +1,23 @@
+#ifndef __TIMER_H__
+#define __TIMER_H__
+#include <signal.h>
+#include <time.h>
+#include <unistd.h>
+
+/*
+USAGE :
+
+run_timer(timer_init(),
+void* function,
+int seconds,
+int mseconds,
+int interval)
+
+*/
+
+timer_t timer_init();
+void timer_deinit();
+void function_register(timer_t* timerID, void* function);
+void time_register(timer_t* timerID, int _sec, int _msec, int _interval);
+void run_timer(timer_t* timerID, void* func, int _sec, int _msec, int _interval);
+#endif
