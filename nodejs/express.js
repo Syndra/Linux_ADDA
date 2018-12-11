0a852d7e8f49e397882a67702252c25c0ecffd51
lenny-kong <forlennykong@gmail.com>
2018-12-12 01:19:00 +0900

    asdad

--- nodejs/express.js
+++ nodejs/express.js
@@ -23,6 +23,10 @@ app.post('/searchwakeuplog', (req, res) => {
   search_op_wakeuplog(req, res);
 });
 
+app.post('/searchavg', (req, res) => {
+  search_op_avg(req, res);
+});
+
 app.listen(3000, () => {
   console.log('Express App on port 3000!');
 });
@@ -97,6 +101,35 @@ function search_op_wakeuplog(request, response)
         console.log(err);
       else{
       fs.readFile('html/wakeuplist.html', 'utf-8', function(error, datas){
+        if(error)
+          console.log('Readfile error');
+        else{
+          response.send(ejs.render(datas, {wakeuplogList:results}));
+        }
+      });
+      }
+    });
+  });
+
+}
+
+function search_op_avg(request, response)
+{
+  var body = '';
+  const chunks = [];
+  var _data;
+  request.on('data', chunk => chunks.push(chunk));
+  request.on('end', () =>
+  {
+    data = qs.parse(Buffer.concat(chunks).toString());
+    var connection = mysql_load();
+    connection.query('SELECT sec_to_time(Alarmoff - Alarmon) as AVG FROM wakeuplog where (Alarmon > ? or Alarmon < ?) and (Temp > ? or Temp < ?) and (Light > ? or Light < ?) and (Alti > ? or Alti < ?) and (Press > ? or Press < ?)',
+    [data.starttime, data.endtime, data.starttemp, data.endtemp, data.startlight, data.endlight, data.startalti, data.endalti, data.startpress, data.endpress],
+    function(err, results){
+      if(err)
+        console.log(err);
+      else{
+      fs.readFile('html/wakeupavg.html', 'utf-8', function(error, datas){
         if(error)
           console.log('Readfile error');
         else{
